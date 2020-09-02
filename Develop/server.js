c8d41df900d7f3043f07082649e352150044d001
ryanbrooks99 <ryanbrooks99@gmail.com>
2020-09-02 17:31:03 -0400

    adjustments to server file

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var dbJSON = require("./db/db.json")
 var fs = require("fs");
+var uuidv1 = require("uuid/v1");
 
 // Sets up the Express App
 var app = express();
@@ -13,6 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+function readFile() {
+  var notes = JSON.parse(fs.readFileSync("db/db.json", "utf8")) || [];
+  console.log(notes);
+  return notes;
+}
+function writeFile(notes) {
+  fs.writeFileSync("db/db.json", JSON.stringify(notes));
+}
 //Setting up routes
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -25,23 +34,27 @@ app.get("/", function(req, res) {
 
   // Displays all notes
   app.get("/api/notes", function(req, res) {
-    res.json(dbJSON);
+    res.json(readFile());
   });
   
   app.post("/api/notes", function(req, res) {
-    var newNotes = req.body;
-
-    dbJSON.push(newNotes);
-
-    res.json(newNotes);
+    var notes = readFile();
+    var title = req.body.title;
+    var text = req.body.text;
+    var newNote = {title, text, id:uuidv1()}
+    notes.push(newNote);
+    writeFile(notes);
+    res.json(newNote);
   });
 
   app.delete("/api/notes/:id", function(req, res) {
-    
+    var notes = readFile();
+    var filteredNotes = notes.filter(note => note.id !== req.params.id)
+    writeFile(filteredNotes)
+    res.json({"Ok": true})
   });
 
   
-
 // Server listening...
   app.listen(PORT, function() {
     console.log("App listening on http://localhost:" + PORT);
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Note Taker
 
-## This application may be used to write, save, and delete notes. 
+## This application may be used to write, save, and delete notes. The app uses Express and API routes.
